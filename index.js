cb292ddff5578970a1d8bd98be5a74ef160c371b
Varzzz123 <varzgeteamabar@gmail.com>
2024-10-17 21:16:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const body = document.body
+let nama = document.querySelector(Varz)
+
+nama.style.background = 'aqua'
+
+function Varz() {
+  let nama = prompt('Siapa Namamu?')
+  nama.textContent = nama
+  body.append(nama)
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+const body = document.body
+const P5 = document.getElementById('P5')
+const table = document.createElement('table')
+const a = document.createElement('a')
+const imageContainer = document.getElementById('imageContainer')
+const img = document.createElement('img')
+const h1 = document.createElement('h1')
+const contentContainer = document.getElementById('content-container')
+
+
+P5.addEventListener('click', function () {
+  window.location.href = 'P5.html';
+})
